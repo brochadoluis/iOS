137197a41cb2a1f3c34f956d5fdd7877491228f6
Luís Brochado Pinto dos Reis <luis@MacBook-Pro-de-Barbara.local>
2019-03-04 09:35:07 +0000

    add hustle mode app

---
+++ hustle-mode/hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Luís Brochado Pinto dos Reis on 03/03/2019.
+//  Copyright © 2019 Luís Brochado Pinto dos Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
