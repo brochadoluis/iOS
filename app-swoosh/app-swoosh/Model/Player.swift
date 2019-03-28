8bc8fc2e3480304e72b2f95f466f8b2433b5e346
Luís Brochado Pinto dos Reis <luis@MacBook-Pro-de-Barbara.local>
2019-03-28 23:31:21 +0000

    add data transition between view controllers

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Luís Brochado Pinto dos Reis on 28/03/2019.
+//  Copyright © 2019 Luís Brochado Pinto dos Reis. All rights reserved.
+//
+
+import Foundation
